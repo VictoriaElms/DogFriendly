1fdb0311fe5821cf987b4f290144dbe82c006e45
Victoria <velms18@gmail.com>
2024-05-30 17:18:57 -0300

    Merge branch 'main' of https://github.com/VictoriaElms/DogFriendly

--- routes/api.py
+++ routes/api.py
@@ -1,6 +1,8 @@
 from flask import request, jsonify, Blueprint
-from database.models import Locations
+from flask_login import current_user, login_required
+from database.models import Favourites, Locations, Users
 from dogfriendly import db
+from routes.views import locations
 
 api = Blueprint("api", __name__)
 
@@ -17,76 +19,48 @@ def get_locations():
                 "coordinates": loc.coordinates,
                 "name": loc.name,
                 "offleash": loc.offleash,
-                "hours": loc.hours, 
-                "outdoorspace": loc.outdoorspace
+                "hours": loc.hours,
+                "outdoorspace": loc.outdoorspace,
             }
             for loc in locations
         ]
     )
 
-
-# API endpoint to save a new location
+# API endpoint for favourites
 @api.route("/api/locations/save", methods=["POST"])
-def save_location():
-    data = request.json
-    new_location = Locations(
-        name=data["name"],
-        address=data["address"],
-        coordinates=data["coordinates"],
-        category=data["category"],
-        offleash=data["offleash"],
-        hours=data["hours"],
-        outdoorspace=data["outdoorspace"],
-    )
-    db.session.add(new_location)
+def save_favourites():
+    location = Locations.query.filter_by(name=request.args.get("id")).all()
+    new_save = Favourites(location_id=location[0].id, user_id=current_user.id)
+    db.session.add(new_save)
     db.session.commit()
-    return jsonify({"message": "Location saved successfully"})
+    return "Location added to favourites"
 
+#API endpoint for show favourites
+@api.route("api/locations/favourites", methods=["GET"])
+def get_favourites():
+    login_required
+    rows= Favourites.query.filter_by(user_id=current_user.id).all()
+    location_ids=[]
+    for row in rows:
+        location_ids.append(row.location_id)
+    locations = Locations.query.filter(Locations.id in location_ids).all()
+    #locations = db.session.query(Locations).filter(Locations.id in location_ids).all()
+    return jsonify(
+        [
+            {
+                "address": loc.address,
+                "category": loc.category,
+                "coordinates": loc.coordinates,
+                "name": loc.name,
+                "offleash": loc.offleash,
+                "hours": loc.hours,
+                "outdoorspace": loc.outdoorspace,
+            }
+            for loc in locations
+        ]
+    )
 
-# API endpoint to filter locations by category
-@api.route("/api/locations/filter", methods=["GET"])
-def filter_locations():
-    category = request.args.get("category")
-    filtered_locations = Locations.query.filter_by(category=category).all()
-    return jsonify([loc.serialize() for loc in filtered_locations])
-
-def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'address': self.address,
-            'category': self.category
-        }
-
-@api.route("/api/locations/search", methods=["GET"])
-def search_locations():
-    query = request.args.get("q", "")
-    if query:
-        results = Locations.query.filter(Locations.name.ilike(f"%{query}%")).all()
-    else:
-        results = []
-    return jsonify([loc.serialize() for loc in results])
-
-
-#@api.route("/api/locations/favorites", methods=["GET"])
-#@login_required
-#def get_favorite_locations():
- #   user_id = current_user.id
-  #  favorite_locations = Locations.query.join(Favorites).filter(Favorites.user_id == user_id).all()
+    #Make this end points: get userID then find all of the matching rows in the favourites table and then get the location ids 
+    #return the rows from the locations table that matches the ids 
+    #write front end function in locations.js that adds locations to the div almost the same as the formatLocations
     
-   # results = []
-    #for location in favorite_locations:
-     #   results.append({
-      #      "id": location.id,
-       #     "name": location.name,
-        #    "address": location.address,
-         #   "category": location.category,
-            # Include any other fields you want to send back
-       # })
-    
-  #  return jsonify(results)
-
-
-# API endpoint to save locations to profiles
-#@api.route("/api/locations/save", methods=["POST"])
-#def save_locations():
