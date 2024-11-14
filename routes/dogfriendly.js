ce8a8fe4e87327480d72be74dae374b305f4d7a3
Victoria <velms18@gmail.com>
2024-11-14 13:37:38 -0400

    MERN update

---
+++ routes/dogfriendly.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+router.get('/places', (req, res) => {
+  // Code to retrieve places from MongoDB
+});
+module.exports = router;
